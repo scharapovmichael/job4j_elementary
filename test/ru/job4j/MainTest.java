b7fd91d3ab542374267fdd7c327fd5a488b1539b
scharapovmichael <scharapovm@yandex.ru>
2021-01-27 11:04:03 +0700

    3.1. Загрузка файлов на github [#447702]

--- test/ru/job4j/MainTest.java
+++ test/ru/job4j/MainTest.java
@@ -1,14 +1,12 @@
 package ru.job4j;
 
-import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import org.testng.annotations.Test;
 
 public class MainTest {
     @Test
     public void whenActionThenResult() {
-        int excepted = 1;
-        int result = 1;
-        assertThat(result, is(excepted));
+//        int excepted = 1;
+//        int result = 1;
+//        assertThat(result, is(excepted));
     }
 }
