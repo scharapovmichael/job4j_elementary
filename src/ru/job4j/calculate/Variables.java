c5922e5f33c039348fd8e899daf03603dfacdf1c
scharapovmichael <scharapovm@yandex.ru>
2021-02-01 12:51:53 +0700

    0. Что такое переменная. [#447691]

---
+++ src/ru/job4j/calculate/Variables.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        float length = 78.0332F;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(true);
+    }
+}
