5913b079a38c2b507d1da6ec01f8afa6a93d7057
scharapovmichael <scharapovm@yandex.ru>
2021-02-13 21:57:29 +0700

    Идеальный вес [#447676]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,12 +2,10 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        short heigt = 187;
-        return (heigt-100)*1.15;
+        return (height-100)*1.15;
     }
     public static double womanWeight(short height) {
-        short heigt = 160;
-         return (heigt-110)*1.15;
+         return (height-110)*1.15;
     }
     public static void main(String[] args) {
         short height = 187;
