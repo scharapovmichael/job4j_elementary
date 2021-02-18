140a3ffc830bafac8aa47bc28421897492db605d
scharapovmichael <scharapovm@yandex.ru>
2021-02-18 19:51:51 +0700

    9.1. String [#447686]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args){
+        String idea = "I like java!";
+        int year = 2021;
+        String id = idea + "But I am a newbie " + year;
+        System.out.println(id);
+    }
+}
