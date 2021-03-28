0103f87ef175817e5a9010acf8b6bb13ccf36f54
scharapovmichael <scharapovm@yandex.ru>
2021-03-28 22:00:20 +0700

    4.3.4. if c return [#447631]

--- src/ru/job4j/condition/PointEq.java
+++ src/ru/job4j/condition/PointEq.java
@@ -1,10 +1,9 @@
 package ru.job4j.condition;
 
 public class PointEq {
-    private static boolean compare;
 
     public static boolean eq(int x1, int y1, int x2, int y2) {
-        return compare = (x1 == x2) && (y1 == y2);
+        return (x1 == x2) && (y1 == y2);
     }
 
 
