f46331d93efbeffb6d2a07a443fcaa2247e67fe7
scharapovmichael <scharapovm@yandex.ru>
2021-05-03 13:57:01 +0700

    5.1. Подсчет суммы чётных чисел в диапазоне [#447741]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -12,7 +12,7 @@ public class Counter {
         int sum = 0;
         for ( int index = start; index <= finish; index++ ){
             if ( index % 2 == 0 ){
-                sum = sum + index++;
+                sum = sum + index;
             }
         }
         return sum;
