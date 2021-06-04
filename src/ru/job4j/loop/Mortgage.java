5cbb9472b2f4f393877e98466c68e952cba1b88b
scharapovmichael <scharapovm@yandex.ru>
2021-06-04 22:16:18 +0700

    5.7. Ипотека [#156315]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        double remainder = amount + amount * (percent / 100);
+        while (remainder > 0) {
+            remainder = remainder - salary;
+            year++;
+        }
+        return year;
+    }
+
+}
+
+
+
