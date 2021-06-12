499bedf6a40271d939d2d7ea99cd4a43b0cc5fa8
scharapovmichael <scharapovm@yandex.ru>
2021-06-12 20:33:43 +0700

    4.6.1. Switch [#334070 #20686]

--- src/ru/job4j/condition/SwitchWeek.java
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -3,34 +3,33 @@ package ru.job4j.condition;
 public class SwitchWeek {
     public static String nameOfDay(int day) {
         String name;
-        switch (day){
-            case 1 :
-                name  = "Понедельник";
+        switch (day) {
+            case 1:
+                name = "Понедельник";
                 break;
-            case 2 :
+            case 2:
                 name = "Вторник";
                 break;
-            case 3 :
+            case 3:
                 name = "Среда";
                 break;
-            case 4 :
+            case 4:
                 name = "Четверг";
                 break;
-            case 5 :
+            case 5:
                 name = "Пятница";
                 break;
-            case 6 :
+            case 6:
                 name = "Суббота";
-            case 7 :
+            case 7:
                 name = "Воскресенье";
-            default :
+            default:
                 name = "Ошибка";
                 break;
         }
         return name;
     }
 
-    public static void main(String[] args) {
 
-    }
 }
+
