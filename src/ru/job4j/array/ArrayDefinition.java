5072bdf157b5ae54d74b7b1b4d29b0499378fd05
scharapovmichael <scharapovm@yandex.ru>
2021-06-12 13:49:53 +0700

    6.0.1. Объявление массива. [#3613 #166931]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,6 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + ages.length);
-        System.out.println("Размер массива равен: " + surname.length);
-        System.out.println("Размер массива равен: " + prices.length);
         }
     }
 
