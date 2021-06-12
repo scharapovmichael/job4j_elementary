c1ce616fbd69e1ca7fdb966c47cd9f68fd2c617f
scharapovmichael <scharapovm@yandex.ru>
2021-06-12 14:08:02 +0700

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,9 +2,15 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surname = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Michael";
+        names[1] = "Sergey";
+        names[2] = "Svetlana";
+        names[3] = "Kristina";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
         }
     }
 
