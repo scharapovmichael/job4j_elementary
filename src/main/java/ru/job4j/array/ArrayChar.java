ac1904c450213346e2fa8946977c3b171bca6b6c
scharapovmichael <scharapovm@yandex.ru>
2021-11-23 17:39:47 +0700

    6.4. Слово начинается с ... [#41585 #229036]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,9 +5,6 @@ public class ArrayChar {
         boolean result = true;
         for (int index = 0; index < pref.length; index++) {
             if (word[word.length - index - 1] != pref[pref.length - index - 1]) {
-                char temp = word[word.length - index - 1];
-                word[word.length - index - 1] = word[index];
-                word[index] = temp;
                 result = false;
                 break;
             }
