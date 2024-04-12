9851877f28606c5fdd46216402231fe6c62704ef
scharapovmichael <scharapovm@yandex.ru>
2024-04-12 19:13:11 +0700

    123

---
+++ src/main/java/ru/job4j/condition/StringRunner.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + " But I am a newbie. ";
+        int year = 2022;
+        idea = idea + year;
+        idea = idea + year;
+        idea = idea + year;
+        idea = idea + year;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
